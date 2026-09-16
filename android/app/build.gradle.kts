6c2acbc3e6c3672ca298dc4e1204b61a782b75f8
mido200566 <60202439+mido200566@users.noreply.github.com>
2026-09-17 01:53:05 +0300

    fix(android): enforce compileSdk 36 across app and subprojects to satisfy lifecycle AAR metadata

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.empos"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "30.0.16248370"
 
     compileOptions {
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -20,6 +20,15 @@ subprojects {
         val android = project.extensions.findByName("android")
         if (android != null) {
             try {
+                val setCompileSdk = android.javaClass.getMethod("compileSdkVersion", Int::class.javaPrimitiveType)
+                setCompileSdk.invoke(android, 36)
+            } catch (_: Exception) {
+                try {
+                    val setCompileSdkStr = android.javaClass.getMethod("compileSdkVersion", String::class.java)
+                    setCompileSdkStr.invoke(android, "android-36")
+                } catch (_: Exception) {}
+            }
+            try {
                 val setNdk = android.javaClass.getMethod("setNdkVersion", String::class.java)
                 setNdk.invoke(android, "30.0.16248370")
             } catch (_: Exception) {}
